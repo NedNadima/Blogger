ee1334018b31ff187a59ff2d47d4b8cb55f32216
NedNadima <nnadima1@gmail.com>
2020-10-29 18:51:56 -0400

    create users table and model

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'nokogiri', '~> 1.6', '>= 1.6.8'
 gem 'bootstrap-sass', '~> 3.4.1'
 gem 'sassc-rails', '>= 2.1.0'
 gem 'jquery-rails'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'about', to: 'pages#about'
   get '/articles', to: 'pages#index'
+  get 'signup', to: 'users#new'
 
 
   resources :articles
+  resources :users, except: [:new]
 end
